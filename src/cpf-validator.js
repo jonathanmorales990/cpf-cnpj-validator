32aeb3bcfa5ddda56d895833740b91c7acca5bdc
John morales <johnmorales990@gmail.com>
2024-04-24 22:24:53 -0300

    Fix code add regex do validate same numbers

--- src/cpf-validator.js
+++ src/cpf-validator.js
@@ -1,18 +1,8 @@
 function validateCPF(value) {
   if (value && value.length >= 11) {
     value = value.replace(/[^a-zA-Z0-9]/g, '');
-    if (
-      value === '00000000000' ||
-      value === '11111111111' ||
-      value === '22222222222' ||
-      value === '33333333333' ||
-      value === '44444444444' ||
-      value === '55555555555' ||
-      value === '66666666666' ||
-      value === '77777777777' ||
-      value === '88888888888' ||
-      value === '99999999999'
-    ) {
+
+    if (/^(\d)\1{10}$/.test(value)) {
       return false;
     }
 
